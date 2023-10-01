0601d1bac9c3da4e05b65d6fbcef16b56b894715
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2023-10-02 01:12:06 +0300

    react query and cors added

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 function App() {
+  const { isLoading, error, data, isFetching } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      axios.get('http://localhost:3001/home').then((res) => {
+        console.log(res);
+        return res.data;
+      }),
+  });
+
+  if (isLoading) return 'Loading...';
+
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
@@ -6,11 +21,13 @@ function App() {
         <div className="card-body">
           <h2 className="card-title">Shoes!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
+          <p>{data}</p>
           <div className="card-actions justify-end">
             <button className="btn btn-primary">Buy Now</button>
           </div>
         </div>
       </div>
+      <ReactQueryDevtools initialIsOpen />
     </>
   );
 }
