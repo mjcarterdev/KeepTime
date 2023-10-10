8f908adb597c244eb51f188faf1cc2009117efcc
anastat <anastasiia.tikhonova@mail.com>
2023-10-10 22:14:56 +0300

    Added app routing.

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,6 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import axios from 'axios';
 import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Login from './views/pages/Login';
+import SignUp from './views/pages/SignUp';
+import Home from './views/pages/Home';
 
 function App() {
   const { isLoading, error, data } = useQuery({
@@ -18,19 +22,12 @@ function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div className="shadow-xl card w-96 bg-base-100">
-        <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
-          <p className="px-4 py-2 text-white sm:px-8 sm:py-3 bg-sky-700 hover:bg-sky-200">
-            If a dog chews shoes whose shoes does he choose?
-          </p>
-          <p>{data}</p>
-          <div className="justify-end card-actions">
-            <button className="btn btn-primary">Buy Now</button>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
       <ReactQueryDevtools initialIsOpen />
     </>
   );
--- src/client/main.jsx
+++ src/client/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 // Create a client
 const queryClient = new QueryClient();
@@ -10,7 +11,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </QueryClientProvider>
   </React.StrictMode>,
 );
