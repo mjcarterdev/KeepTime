79e69fc3b27cab10621a79fb28f9555aa5b73d12
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2023-10-11 05:35:32 +0000

    testing deployment action

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <div className="shadow-xl card w-96 bg-base-100">
         <div className="card-body">
-          <h2 className="card-title">Shoes!</h2>
+          <h2 className="card-title">Testing Deployment Actions</h2>
           <p className="px-4 py-2 text-white sm:px-8 sm:py-3 bg-sky-700 hover:bg-sky-200">
             If a dog chews shoes whosed shoes does he choose?
           </p>
