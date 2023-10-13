867c16cfd6955fa931dd68e17e55e5bdb4825f8e
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2023-10-14 01:26:15 +0300

    stufff

--- src/client/pages/SignUpPage.jsx
+++ src/client/pages/SignUpPage.jsx
@@ -1,7 +1,28 @@
 import Logo from '../components/Logo';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useEffect } from 'react';
 
 const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    clearErrors,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  const hidden = 'invisible label-text-alt';
+  const visible = 'label-text-alt';
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      clearErrors();
+    }, 10000);
+    return () => clearTimeout(timer);
+  }, [errors]);
+
   return (
     <>
       <div className="h-[calc(100vh-4rem)] hero bg-base-100">
@@ -13,13 +34,43 @@ const SignUpPage = () => {
               <br />
               Your Time. Your Way.
             </p>
-            <div className="flex justify-evenly">
-              <button className="w-24 btn btn-primary">
-                <Link to={'/'}>Back</Link>
-              </button>
-              <button className="w-24 btn btn-primary">
-                <Link to={'/projects'}>Sign Up</Link>
-              </button>
+            <form className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="w-full input input-bordered input-primary"
+                {...register('email', { required: true, maxLength: 20 })}
+              />
+              <label className="label">
+                <span className={errors.email ? visible : hidden}>This is required</span>
+              </label>
+
+              <label className="pt-2 label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Type here"
+                className="w-full input input-bordered input-primary"
+                {...register('password', { required: true, minLength: 8 })}
+              />
+              <label className="label">
+                <span className={errors.password ? visible : hidden}>Min. of 8 characters</span>
+              </label>
+            </form>
+            <div className="flex pt-4 justify-evenly">
+              <Link to={'/'}>
+                <button className="w-24 btn btn-primary">Back</button>
+              </Link>
+
+              <Link to={'/projects'}>
+                <button type="submit" onClick={handleSubmit(onSubmit)} className="w-24 btn btn-primary">
+                  Sign Up
+                </button>
+              </Link>
             </div>
           </div>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const { PORT = 3001 } = process.env;
 
 // https://vitejs.dev/config/
 export default defineConfig({
