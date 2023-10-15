e025b11e1db2bc968e7a005744df840667cb2393
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2023-10-15 21:25:16 +0000

    fix maybe?

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@babel/plugin-syntax-import-assertions'],
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 300,
     rollupOptions: {
