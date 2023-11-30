b84e02e24b43e6f2be932fcfcf7896867a1e518d
Michael Carter <53894470+Carter-Prime@users.noreply.github.com>
2023-11-30 21:51:43 +0000

    Revert "sign up mod"
    This reverts commit 11adc0a9393c2b9fa4865558cf5bcc8aab23a64a.

--- src/client/pages/SignUpPage.jsx
+++ src/client/pages/SignUpPage.jsx
@@ -179,6 +179,16 @@ const SignUpPage = () => {
             </p>
           </form>
         </Card>
+        {errorMessage && (
+          <div
+            className="cursor-pointer toast-top toast-end"
+            onClick={() => setErrorMessage(null)}
+          >
+            <div className="alert alert-error">
+              <span>{errorMessage}</span>
+            </div>
+          </div>
+        )}
       </div>
       <ToastContainer />
     </>
